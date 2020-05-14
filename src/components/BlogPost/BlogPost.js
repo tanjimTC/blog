fbad4c1f2dbaa4946e452551e85632cbdb383b77
tanjimTC <tanjimtc74@gmail.com>
2020-05-14 18:19:07 +0600

    post page updated

--- src/components/BlogPost/BlogPost.js
+++ src/components/BlogPost/BlogPost.js
@@ -1,31 +1,48 @@
-import React,{ useEffect } from "react";
-import './BlogPost.css';
-import blogPost from '../../data/blog.json';
+import React, { useEffect, useState } from "react";
+import "./BlogPost.css";
+import blogPost from "../../data/blog.json";
 
-const BlogPost = ({props}) => {
+const BlogPost = ({ props }) => {
+  const [post, setPost] = useState({
+    id: "",
+    blogCategory: "",
+    blogTitle: "",
+    postedOn: "",
+    author: "",
+    blogImage: "",
+    blogText: "",
+  });
+  useEffect(() => {
+    const postId = props.match.params.postId;
+    const post = blogPost.data.find((x) => x.id === parseInt(postId));
+    setPost(post);
+    const imageName = post.blogImage;
+    console.log(imageName);
+  }, [props.match.params.postId]);
 
-    // const [post, setPost] = useState()
-    useEffect(()=>{
-        const postId = props.match.params.postId;
-        console.log(postId);
-        const post =blogPost.data.find(x=> x.id === parseInt(postId));
-        console.log(post);
-    },[])
-    return (
-        <div className="blogPost">
-            <div className='blogHeader'>
-                <span className='blogCategory'>Featured</span>
-                <h1 className='postTitle'>Fitness Mantra To Live Fit Life</h1>
-                <span className='postedBy'>posted on July 21, 2016 by Sora Blogging Tips</span>
-            </div>
-            <div className="postImageContainer">
-                <img src={require('../../blogPostImages/memories-from.jpg')} alt="Post Image"/>
-            </div>
-            <div className="postContent">
-                
-            </div>
-        </div>
-    );
-};  
+  if (post.blogImage === "") return null;
+
+  return (
+    <div className="blogPost">
+      <div className="blogHeader">
+        <span className="blogCategory">{post.blogCategory}</span>
+        <h1 className="postTitle">{post.blogTitle}</h1>
+        <span className="postedBy">
+          posted on {post.postedOn} by {post.author}
+        </span>
+      </div>
+      <div className="postImageContainer">
+        <img
+          src={require("../../blogPostImages/" + post.blogImage)}
+          alt="Post"
+        />
+      </div>
+  <h3>{post.slug}</h3>
+      <div className="postContent">
+        <p>{post.blogText}</p>
+      </div>
+    </div>
+  );
+};
 
 export default BlogPost;
--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SideBar.css";
 import Card from "../UI/Card/Card";
+import blogPost from "../../data/blog.json";
+import { NavLink } from "react-router-dom";
 
 const SideBar = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const post = blogPost.data;
+    setPosts(post);
+    console.log("sidebar", post);
+  }, []);
+
   return (
     <div className="sideBar">
       <Card>
@@ -37,18 +47,16 @@ const SideBar = (props) => {
             <span>Recent Post</span>
           </div>
           <div className="recentPosts">
-            <div className="recentPost">
-              <h3>Fitness Mantra To live Fit Life</h3>
-              <span>JUL 21 2016</span>
-            </div>
-            <div className="recentPost">
-              <h3>Beautiful & Special Moment</h3>
-              <span>JUL 21 2016</span>
-            </div>
-            <div className="recentPost">
-              <h3>Beauty Lies Within Special</h3>
-              <span>JUL 21 2016</span>
-            </div>
+            {posts.map((x) =>{ 
+              return (
+                <NavLink className='navlink' to={`/post/${x.id}`} key={x.id}>
+                  <div className="recentPost">
+                    <h3>{x.blogTitle}</h3>
+                    <span>{x.postedOn}</span>
+                  </div>
+                </NavLink>
+              );
+            })}
           </div>
         </div>
       </Card>
