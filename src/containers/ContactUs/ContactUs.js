6c18ff99d3bb53d370377d3cde2770117d2bab6f
tanjimTC <tanjimtc74@gmail.com>
2020-05-17 03:19:25 +0600

    made responsive

--- src/containers/ContactUs/ContactUs.js
+++ src/containers/ContactUs/ContactUs.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import happyImage from '../../blogPostImages/giphy.gif';
+import './ContactUs.css';
 
 const ContactUs = () => {
     return (
-        <div>
-            contact us
+        <div className='contact'>
+            <center>
+            <h3>Developer is sleeping</h3>
+            <img src={happyImage} alt="happy"/>
+            </center>
         </div>
     );
 };
